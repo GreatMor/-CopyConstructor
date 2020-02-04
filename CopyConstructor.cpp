2f945ec69693be2e4a0ab08dde40e83f322231dd
Dmitriy <45266827+GreatMor@users.noreply.github.com>
2020-02-04 16:32:21 +0300

    Add files via upload

---
+++ CopyConstructor.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+class MyClass
+{
+    int* data;
+    int Size;
+
+public:
+    MyClass(int size)
+    {
+        this->Size = size;
+        this->data = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            data[i] = i;
+        }
+        cout << "constructor called \t " << this << endl;
+    };
+
+    MyClass(const MyClass& other)//Copy Constructor
+    {
+        this->Size = other.Size;
+
+        this->data = new int[other.Size]; // create new array
+
+        for (int i = 0; i < other.Size; i++)
+        {
+            this->data[i] = other.Size;// copy data from the old array to the new
+        }
+        cout << "copy constructor called \t" << this << endl;
+    }
+    ~MyClass()
+    {
+        delete[] data;
+        cout << "destructor called \t " << this << endl;
+    }
+};
+
+int main()
+{
+    
+    MyClass A(1);
+    MyClass B(A);
+
+    return 0;
+}
+
